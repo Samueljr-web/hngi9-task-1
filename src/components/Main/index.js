a95a4d2ce00099daa48bfc910865fe1dde5ff4c3
Samuel <samueladeyemi2006@gmail.com>
2022-10-28 04:44:48 +0100

    project cleanup

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,15 +3,24 @@ import '../../styles/main.css'
 import Profile__img from '../../assets/profile__img.png'
 import Github from '../../assets/github.png'
 import Slack from '../../assets/slack.png'
+import Share_icon from '../../assets/share.png'
+import Ellipses_icon from '../../assets/ellipses.png'
 
 export default function index({Link}) {
   return (
     <div className='main'>
-      <div className='profile'>
+      <div className='profile_wrapper'>
+        <div className='profile'>
         <img src={Profile__img} id='profile__img' alt='profile_img' />
         <h2>Samuel Adeyemi</h2>
         {/* <a href='' id='slack'>samueljr-web</a> */}
+       </div>
+       <div className='all_btn'>
+        <a href='/' className='share_btn'><img src={Share_icon} alt="share-btn"/></a>
+        <a href='/' className='ellipse_btn'><img src={Ellipses_icon} alt="ellipse-btn"/></a>
+       </div>
       </div>
+      
       <div className='links'>
         {Link.map((link, id) =>{
           return(
